00b925c2ec0d23483dd39fe9d02a6c63c1612af0
Swr <swr100@wp.pl>
2022-12-29 17:20:54 +0100

    improvement of the html's elements setup and preparation for basic calculations

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let numbers = 9;
-let calculatorContainer = document.getElementById("calculatorContainer")
+let buttonContainer = document.getElementById("buttonContainer")
 for(let i = 0; i <= numbers;i++){
     let number = document.createElement("button")
     console.log(i)
     let numberName = document.createTextNode(i)
+    number.className = "button"
     number.append(numberName)
-    calculatorContainer.append(number)
+    buttonContainer.append(number)
 }
+buttons = Array.from(document.querySelectorAll("button"))
+buttons.forEach(button => button.addEventListener("click",()=>console.log(button.textContent)))
+console.log(buttons)

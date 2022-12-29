a6566647c2d2cff2839ffa6c728fc8bf0e45b3ce
Swr <swr100@wp.pl>
2022-12-29 17:30:54 +0100

    the buttons now can display the numbers on screen

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 let numbers = 9;
 let buttonContainer = document.getElementById("buttonContainer")
+let result = document.getElementById("result")
+//create a button for every possible number
 for(let i = 0; i <= numbers;i++){
+    //create a button
     let number = document.createElement("button")
-    console.log(i)
+    //assign a number to each button
     let numberName = document.createTextNode(i)
+    //add a class 
     number.className = "button"
+    //append each number to a button
     number.append(numberName)
+    //append each button to the container
     buttonContainer.append(number)
 }
-buttons = Array.from(document.querySelectorAll("button"))
-buttons.forEach(button => button.addEventListener("click",()=>console.log(button.textContent)))
-console.log(buttons)
+//create an array of all the buttons
+buttons = Array.from(document.querySelectorAll("button"));
+//for each button add an event listener
+buttons.forEach(button => button
+    //if the button is clicked
+    .addEventListener("click",
+    //append the number to the result
+    ()=>result.append(button.textContent)));

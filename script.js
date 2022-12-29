ff109a61979135bc7dd4557369aa409d43f07f44
Swr <swr100@wp.pl>
2022-12-29 22:29:17 +0100

    +-:x added as buttons

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let numbers = 9;
 let buttonContainer = document.getElementById("buttonContainer")
 let result = document.getElementById("result")
+
+
 //create a button for every possible number
 for(let i = 0; i <= numbers;i++){
     //create a button
@@ -14,6 +16,11 @@ for(let i = 0; i <= numbers;i++){
     //append each button to the container
     buttonContainer.append(number)
 }
+createAbutton(":")
+createAbutton("x")
+createAbutton("+")
+createAbutton("-")
+createAbutton("=")
 //create an array of all the buttons
 buttons = Array.from(document.querySelectorAll("button"));
 //for each button add an event listener
@@ -22,3 +29,15 @@ buttons.forEach(button => button
     .addEventListener("click",
     //append the number to the result
     ()=>result.append(button.textContent)));
+
+function createAbutton(name){
+    let bttn = document.createElement("button")
+    //assign a name to a button
+    let bttnName = document.createTextNode(name)
+    //add a class 
+    bttn.className = "button"
+    //append a name to a button
+    bttn.append(bttnName)
+    //append  button to the container
+    buttonContainer.append(bttn)
+}
